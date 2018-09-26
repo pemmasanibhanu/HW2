aae1a6c276fb149d57f84f49deccbab5d47a08bd
shumsss19 <shumsss19@gmail.com>
2018-09-26 23:50:14 +0300

    Add content of third tab - adding new to-do element

--- javascripts/app.js
+++ javascripts/app.js
@@ -9,27 +9,38 @@ var main = function() {
         "Finish writing this book"
 	];
 	$(".tabs a span").toArray().forEach(function(element) {
-		$(element).on("click", function() {
-			var $element = $(element),
-				$content = $("<ul>");
+		var $element = $(element);
+		$element.on("click", function() {
+			var $content;
 			$(".tabs a span").removeClass("active");
 			$element.addClass("active");
 			$("main .content").empty();
 			if($element.parent().is(":nth-child(1)")) {
+				$content = $("<ul>");
 				toDos.forEach(function (todo) {
 					$content.prepend($("<li>").text(todo));
 				});
-				$("main .content").append($content);
 			}
 			else if ($element.parent().is(":nth-child(2)")) {
+				$content = $("<ul>");
 				toDos.forEach(function (todo) {
 					$content.append($("<li>").text(todo));
 				});
-				$("main .content").append($content);
 			}
 			else if ($element.parent().is(":nth-child(3)")) {
-				console.log();
+				var $input = $("<input>"),
+                	$button = $("<button>").text("+");
+
+                $button.on("click", function () {
+                    if ($input.val() !== "") {
+                        toDos.push($input.val());
+                        $input.val("");
+                    }
+                });
+
+                $content = $("<div>").append($input, $button);
 			}
+			$("main .content").append($content);
 			return false;
 		});
 	});
